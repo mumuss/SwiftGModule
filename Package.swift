3cab296f271122dd0341f80b01c01067364ccad0
mumuss <michel.musslin@gmail.com>
2019-10-03 16:44:40 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "GModule", targets: ["GModule"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGLib.git", .branch("master"))
+        .package(url: "https://github.com/mumuss/SwiftGLib.git", .branch("master"))
     ],
     targets: [
         .target(name: "GModule", dependencies: ["GLib"]),
